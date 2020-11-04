6e17da60e1c5cf8f0ff6d645a548695566cf6adc
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2020-11-04 18:18:15 +0000

    rtte

---
+++ contactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+    if (isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $mailFrom = $_POST['mail'];
+        $message = $_POST['message'];
+
+        $mailTo = "alex.coldicott@outlook.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have received and email from ".$name.".\n\n".$message:
+        
+        mail($mailTo, $txt, $headers);
+        header("Location: index.php?mailsend");
+    }
+
+?>
